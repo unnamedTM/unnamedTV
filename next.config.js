9c21beeb664c2da1fe308b29c830b7d3d33166a6
kappaC0dex <kappanewfrog@gmail.com>
2021-03-30 00:10:47 +0300

    x
    x

--- next.config.js
+++ next.config.js
@@ -1,43 +1,44 @@
 const withImages = require('next-images')
-module.exports = withImages({})
-module.exports = {
-    async redirects() {
-        return [
-            
-            {
-                source: '/members/:slug/twitter',
-                destination: 'https://twitter.com/:slug',
-                permanent: true,
-            },
-            {
-                source: '/members/:slug/instagram',
+module.exports =
+    module.exports = withImages({}), {
 
-                destination: 'https://instagram.com/:slug',
-                permanent: true,
-            },
-            {
-                source: '/members/:slug/github',
+        async redirects() {
+            return [
 
-                destination: 'https://github.com/:slug',
-                permanent: true,
-            },
-            {
-                source: '/members/:slug/twitch',
+                {
+                    source: '/members/:slug/twitter',
+                    destination: 'https://twitter.com/:slug',
+                    permanent: true,
+                },
+                {
+                    source: '/members/:slug/instagram',
 
-                destination: 'https://twitch.tv/:slug',
-                permanent: true,
-            },
-            {
-                source: '/discord',
-                destination: 'https://discord.gg/naHDBvYdYv',
-                permanent: true,
-            },
-            {
-                source: '/members/:slug/youtube',
+                    destination: 'https://instagram.com/:slug',
+                    permanent: true,
+                },
+                {
+                    source: '/members/:slug/github',
 
-                destination: 'https://youtube.com/channel/:slug',
-                permanent: true,
-            },
-        ]
-    },
-}
+                    destination: 'https://github.com/:slug',
+                    permanent: true,
+                },
+                {
+                    source: '/members/:slug/twitch',
+
+                    destination: 'https://twitch.tv/:slug',
+                    permanent: true,
+                },
+                {
+                    source: '/discord',
+                    destination: 'https://discord.gg/naHDBvYdYv',
+                    permanent: true,
+                },
+                {
+                    source: '/members/:slug/youtube',
+
+                    destination: 'https://youtube.com/channel/:slug',
+                    permanent: true,
+                },
+            ]
+        },
+    }
