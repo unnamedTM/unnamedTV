4c3e22ab36945c717e1772cc19b54e77663f1633
kappac0dex <kappac0dex@gmail.com>
2021-07-02 22:45:04 +0300

    x

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,79 +1,79 @@
-import Head from 'next/head';
-import React from 'react'
-import Layout from '../utils/comps/Layout'
-import { FiFrown } from "react-icons/fi";
-import Image from 'next/image'
-import Link from 'next/link';
-import four from '../styles/404.module.css'
+import Head from "next/head";
+import React from "react";
+import Layout from "../utils/comps/Layout";
+
+import Image from "next/image";
+import Link from "next/link";
+import four from "../styles/404.module.css";
 const NotFound = () => {
-    return (
-      <>
-        <Head>
-          <title>Not Found | 404 </title>
-          <script>document.body.style.backgroundColor = "white"</script>
-          <link rel="shortcut icon" href="/images/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta />
-        </Head>
-        <Layout>
+  return (
+    <>
+      <Head>
+        <title>Not Found | 404 </title>
+        <script>document.body.style.backgroundColor = "white"</script>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta />
+      </Head>
+      <Layout>
+        <div
+          className="DaContainer"
+          style={{
+            filter: "drop-shadow(-6px 1px 4px #00000070)",
+            height: "1000px",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            //   justifyContent: "center",
+            alignContent: "center",
+            color: "black",
+            fontSize: "20px",
+          }}
+        >
           <div
             style={{
-              height: "1000px",
+              paddingTop: "100px",
               display: "flex",
-              flexDirection: "column",
-              textAlign: "center",
-              //   justifyContent: "center",
-              alignContent: "center",
-              color: "black",
-              fontSize: "20px",
+              flexDirection: "row",
             }}
           >
+            <Image
+              className={four.Image}
+              src="/images/m-l-v2.svg"
+              alt="YankeeWithBrim"
+              width="100"
+              height={110}
+              priority
+            />
             <div
               style={{
-                paddingTop: "100px",
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
+                textAlign: "center",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              <Image
-              className={four.Image}
-                src="/images/m-l-v2.svg"
-                alt="YankeeWithBrim"
-                width="100"
-                height={110}
-                
-                priority
-              />
               <div
                 style={{
                   display: "flex",
-                  flexDirection: "column",
+                  flexDirection: "row",
                   textAlign: "center",
                   alignItems: "center",
                   justifyContent: "center",
                 }}
               >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    textAlign: "center",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  404 | Not Found <FiFrown style={{ marginLeft: "5px" }} />
-                </div>
-                <div className={four.Link}>
-                  <Link href="/">Go back</Link>
-                </div>
+                404 | Not Found 😔
+              </div>
+              <div className={four.Link}>
+                <Link href="/">Go back</Link>
               </div>
             </div>
           </div>
-        </Layout>
-      </>
-    );
-}
-
+        </div>
+      </Layout>
+    </>
+  );
+};
 
 export default NotFound;
