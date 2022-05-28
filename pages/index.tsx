ed51292f0debf40421c44689271f4e6793e46d84
jvkonstantin <kappac0dex@gmail.com>
2022-05-29 00:20:54 +0300

    |

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Home: NextPage = () => {
     <Nav>
       <main className="flex justify-center items-center flex-col">
         <h3 className="font-bold text-center max-w-xl">
-          We are very sorry. We are under maintence. <br /> Please come back at
-          a later time <br /> {"(*_ _)人"}
+          We are very sorry. We are under maintenance. <br /> Please come back
+          at a later time <br /> {"(*_ _)人"}
         </h3>
         <div className="max-w-2xl">
           <Image src={Offline} className="max-h-1" alt="offline gif" />
